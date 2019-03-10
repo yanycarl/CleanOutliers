3d931d2ce15e04be6da95022fd8352d12a323e83
YanyCarl <33498928+yanycarl@users.noreply.github.com>
2019-03-10 00:00:54 +0000

    Add files via upload

---
+++ Weighted Error.py
@@ -0,0 +1,6 @@
+
+from CustomLibs.WeightedError import WeightedError
+
+instance = WeightedError()  # Must write
+result = instance.calculateError(date=[], y1=[], y2=[])
+# Type: date, y1, y2 are lists
