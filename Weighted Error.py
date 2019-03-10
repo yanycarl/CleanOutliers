ca8f76df5a3c5d799c0d7a0a34019dc5fff3498c
YanyCarl <33498928+yanycarl@users.noreply.github.com>
2019-03-10 00:01:36 +0000

    Delete Weighted Error.py

--- Weighted Error.py
+++
@@ -1,6 +0,0 @@
-
-from CustomLibs.WeightedError import WeightedError
-
-instance = WeightedError()  # Must write
-result = instance.calculateError(date=[], y1=[], y2=[])
-# Type: date, y1, y2 are lists
